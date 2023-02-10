87f58d5a454bbc8e45de1659afe2e5694edfa8e3
Frytak <frytak2855@gmail.com>
2023-02-10 16:56:27 +0100

    Main almost done. Starting with search.

---
+++ main/main.js
@@ -0,0 +1,9 @@
+/** @format */
+
+console.log('Hello from popup.js');
+// let style = document.createElement('style');
+// style.innerHTML = '#sb_form { background-color: red !important;}';
+// document.head.appendChild(style);
+
+let hideShowCaro = document.getElementById('hideShowCaro');
+hideShowCaro.classList.remove('down');
